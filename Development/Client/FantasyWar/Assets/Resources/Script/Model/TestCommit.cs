8b171e0965cc1f9041152aa78aaa232f013a13b0
Checkmate <programmer_ljz@outlook.com>
2017-09-29 12:23:13 +0800

    ...

--- Development/Client/FantasyWar/Assets/Resources/Script/Model/TestCommit.cs
+++ Development/Client/FantasyWar/Assets/Resources/Script/Model/TestCommit.cs
@@ -7,6 +7,7 @@ public class TestCommit : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		//sgfghfgfhhgfghfgfghfghhgffhg 李俊佐在测试跟郑康宇的代码冲突
+		//我加多了一行测试冲突
 	}
 	
 	// Update is called once per frame
