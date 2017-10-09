dde7852b09f3805c0006d2fe70f38c65c66c1646
finalsola <122825885@qq.com>
2017-10-09 20:58:59 +0800

    普通单例模板

---
+++ Development/Client/FantasyWar/Assets/Resources/Script/CommonTool/UnitySingleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Singleton<T> where T : Singleton<T>, new()
+{
+    private static T _shareInstance;
+    private static readonly object syslock = new object();
+
+    public static T ShareInstanceance
+    {
+        get
+        {
+            if (_shareInstance == null)
+            {
+                //lock (syslock)
+                //{
+                //    if (_shareInstance == null)
+                //    {
+                _shareInstance = SingletonManager.AddSigleton<T>();
+                //    }
+                //}
+            }
+            return _shareInstance;
+        }
+    }
+}
---
+++ Development/Client/FantasyWar/Assets/Resources/Script/CommonTool/UnitySingleton/SingletonManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public static class SingletonManager
+{
+    private static ArrayList SingletonList = new ArrayList();
+
+    public static T AddSigleton<T>() where T:Singleton<T>,new()
+    {
+        T tmp = new T();
+        SingletonList.Add(tmp);
+        return tmp;
+    }
+
+    //public static void RemoveSingleton<T>() where T: Singleton<T>, new()
+    //{
+    //    int tmp=-1;
+    //    for (int i = 0; i < SingletonList.Count; i++)
+    //    {
+    //        if (SingletonList[i] is T)
+    //            tmp = i;
+    //    }
+    //}
+}
