8a47f8af96feb66eff9e3fc9fe4d7684c8858f04
finalsola <122825885@qq.com>
2017-10-10 21:01:56 +0800

    增加MonoBehavior的对象池

--- Development/Client/FantasyWar/Assets/Resources/Script/CommonTool/PoolManager/PoolManager.cs
+++ Development/Client/FantasyWar/Assets/Resources/Script/CommonTool/PoolManager/PoolManager.cs
@@ -71,60 +71,118 @@ public class PoolManager {
 		if (itemList == null || !itemList.ContainsKey (_name))  
 			PushData (_name);//添加对象组  
 		//添加对象  
-		itemList [_name].PushObject (_gameObject);  
-	}  
+		((PoolItemGameObject)itemList [_name]).PushObject (_gameObject);  
+	}
 
-	/// <summary>  
-	/// 移除单个对象，真正的销毁!!  
-	/// </summary>  
-	public static void RemoveObject(string _name, GameObject _gameObject){  
+    /// <summary>  
+    /// 添加单个对象（首先寻找对象组->添加单个对象）  
+    /// </summary>  
+    public static void PushObject(string _name, MonoBehaviour _mono)
+    {
+        if (itemList == null || !itemList.ContainsKey(_name))
+            PushData(_name);//添加对象组  
+                            //添加对象  
+        ((PoolItemMonoBehavior)itemList[_name]).PushObject(_mono);
+    }
+
+    /// <summary>  
+    /// 移除单个对象，真正的销毁!!  
+    /// </summary>  
+    public static void RemoveObject(string _name, GameObject _gameObject){  
 		if (itemList == null || !itemList.ContainsKey (_name))  
-			return;  
-		itemList [_name].RemoveObject (_gameObject);  
-	}  
+			return;
+        ((PoolItemGameObject)itemList[_name]).RemoveObject (_gameObject);  
+	}
 
-	/// <summary>  
-	/// 获取缓存中的对象  
-	/// </summary>  
-	public static GameObject GetObject(string _name){  
+    /// <summary>  
+    /// 移除单个对象，真正的销毁!!  
+    /// </summary>  
+    public static void RemoveObject(string _name, MonoBehaviour _mono)
+    {
+        if (itemList == null || !itemList.ContainsKey(_name))
+            return;
+        ((PoolItemMonoBehavior)itemList[_name]).RemoveObject(_mono);
+    }
+
+    /// <summary>  
+    /// 获取缓存中的对象  
+    /// </summary>  
+    public static object GetObject(string _name){  
 		if (itemList == null || !itemList.ContainsKey (_name)) {  
 			return null;  
 		}  
-		return itemList [_name].GetObject ();  
-	}  
+        if(itemList[_name] is PoolItemGameObject)
+		return ((PoolItemGameObject)itemList[_name]).GetObject ();
+        if(itemList[_name] is PoolItemMonoBehavior)
+        return ((PoolItemMonoBehavior)itemList[_name]).GetObject();
+        return null;
+	}
 
-	/// <summary>  
-	/// 销毁对象，没有真正的销毁!!  
-	/// </summary>  
-	public static void DestroyActiveObject(string _name, GameObject _gameObject){  
+
+
+    /// <summary>  
+    /// 销毁对象，没有真正的销毁!!  
+    /// </summary>  
+    public static void DestroyActiveObject(string _name, GameObject _gameObject){  
 		if (itemList == null || !itemList.ContainsKey (_name)) {  
 			return;  
-		}  
-		itemList [_name].DestoryObject (_gameObject);  
-	}  
+		}
+        ((PoolItemGameObject)itemList[_name]).DestoryObject (_gameObject);  
+	}
 
-	/// <summary>  
-	/// 处理超时对象  
-	/// </summary>  
-	public static void BeyondTimeObject(){  
+    /// <summary>  
+    /// 销毁对象，没有真正的销毁!!  
+    /// </summary>  
+    public static void DestroyActiveObject(string _name, MonoBehaviour _mono)
+    {
+        if (itemList == null || !itemList.ContainsKey(_name))
+        {
+            return;
+        }
+        ((PoolItemMonoBehavior)itemList[_name]).DestoryObject(_mono);
+    }
+
+    /// <summary>  
+    /// 处理超时对象  
+    /// </summary>  
+    public static void BeyondTimeObject(){  
 		if (itemList == null) {  
 			return;  
 		}  
-		foreach (PoolItem poolI in itemList.Values) {  
-			poolI.BeyondObject ();  
+		foreach (PoolItem poolI in itemList.Values) {
+            if (poolI is PoolItemGameObject)
+            {
+                ((PoolItemGameObject)poolI).BeyondObject();
+                return;
+            }
+            if (poolI is PoolItemMonoBehavior)
+            {
+                ((PoolItemMonoBehavior)poolI).BeyondObject();
+                return;
+            }
+              
 		}  
-	}  
+	}
 
-	/// <summary>  
-	/// 销毁对象，真正的销毁!!  
-	/// </summary>  
-	public static void Destroy(){  
+    /// <summary>  
+    /// 销毁全部对象，真正的销毁!!  
+    /// </summary>  
+    public static void Destroy(){  
 		if (itemList == null) {  
 			return;  
 		}  
-		foreach (PoolItem poolI in itemList.Values) {  
-			poolI.Destory ();  
-		}  
+		foreach (PoolItem poolI in itemList.Values) {
+            if (poolI is PoolItemGameObject)
+            {
+                ((PoolItemGameObject)poolI).Destory();
+                return;
+            }
+            if (poolI is PoolItemMonoBehavior)
+            {
+                ((PoolItemMonoBehavior)poolI).Destory();
+                return;
+            }
+        }  
 		itemList = null;  
 	}  
 
