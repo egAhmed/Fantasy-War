3e3cac2dce63ecfe103ee6f5b22223afffe2c599
finalsola <122825885@qq.com>
2017-10-12 14:32:26 +0800

    Shader快速加载工具

---
+++ Development/Client/FantasyWar/Assets/Editor/ShaderTool/ChangeShader.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class ChangeShader : EditorWindow
+{
+
+    [MenuItem("Window/ChangeShader/BuidingDestoryShader")]
+    static void BuidingShader()
+    {
+        changeShader(0, @"LCH/BuidingProgressShader");
+        changeShader(1, @"LCH/UnitDestoryShader");
+    }
+
+    //[MenuItem("ChangeShader /BuidingDestoryShader")]
+    //static void BuidingProgressShader()
+    //{
+
+    //}
+
+    //[MenuItem("ChangeShader/UnitBornShader")]
+    //static void UnitBornShader()
+    //{
+
+    //}
+
+    //[MenuItem("ChangeShader/UnitDestoryShader")]
+    //static void UnitDestoryShader()
+    //{
+
+    //}
+
+    /// <summary>
+    /// 传入shader路径，执行shader替换
+    /// </summary>
+    /// <param name="shaderName"></param>
+    /// <returns></returns>
+    static void changeShader(int materialIndex, string shaderName)
+    {
+        if (Selection.activeObject == null)
+        {
+            Debug.LogError("请选择对象");
+            return;
+        }
+
+        string path;
+        path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        Debug.Log(path);
+        if (string.IsNullOrEmpty(path))
+        {
+            Debug.LogError("请在Project中选择对象");
+            return;
+        }
+
+        if (!path.EndsWith("prefab"))
+        {
+            Debug.LogError("请选择perfab对象");
+            return;
+        }
+
+        //标记预设为修改状态 这步很重要，不执行这步操作。关闭Unity后。预设并不会保存。
+        EditorUtility.SetDirty(Selection.activeObject);
+        GameObject obj = Resources.Load<GameObject>((path.Replace(@"Assets/Resources/", "")).Replace(".prefab", "")) as GameObject;
+        Debug.Log((path.Replace(@"Assets/Resources/", "0")).Replace(".prefab", ""));
+        if (obj == null)
+        {
+            Debug.LogError("无法加载该prefab");
+            return;
+        }
+        MeshRenderer tmpMeshR = obj.GetComponent<MeshRenderer>();
+        if (tmpMeshR == null)
+        {
+            Debug.LogError("该prefab没有MeshRenderer组件");
+            return;
+        }
+        MeshFilter tmpMeshF = obj.GetComponent<MeshFilter>();
+        if (tmpMeshF == null)
+        {
+            Debug.LogError("该prefab没有MeshFilter组件");
+            return;
+        }
+
+        if (Shader.Find(shaderName) == null)
+        {
+            Debug.LogError("找不到create时用的Shader");
+            return;
+        }
+        try
+        {
+            tmpMeshR.sharedMaterials[materialIndex].shader = Shader.Find(shaderName);
+
+        }
+        catch
+        {
+            Debug.LogError("无法获取对应Index下的Material");
+            return;
+        }
+
+        float MaxY = -Mathf.Infinity;
+        float MinY = Mathf.Infinity;
+        foreach (var item in tmpMeshF.sharedMesh.vertices)
+        {
+            if (item.y > MaxY)
+            {
+                MaxY = item.y;
+            }
+
+            if (item.y < MinY)
+            {
+                MinY = item.y;
+            }
+        }
+        tmpMeshR.sharedMaterial.SetFloat("_MeshVertexYMax", MaxY);
+        tmpMeshR.sharedMaterial.SetFloat("_MeshVertexYMin", MinY);
+        //如何获取default贴图
+        // tmpMeshR.sharedMaterial.SetTexture("",)
+        obj = null;
+        //清不掉obj
+        Resources.UnloadUnusedAssets();
+        Debug.Log(Selection.activeObject.name + "修改Shader成功");
+    }
+}
---
+++ Development/Client/FantasyWar/Assets/Resources/Shader/BuidingProgressShader.shader
@@ -0,0 +1,89 @@
+﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+
+// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "LCH/BuidingProgressShader" 
+{
+	Properties 
+	{
+		//_Color ("Color", Color) = (1,1,1,1)
+		_BaseTex ("Base (RGB)", 2D) = "white" {}
+		//建造进度
+		_Progress ("Progress", Range(0,1)) = 1
+		//C#中通过遍历Mesh.vertex，选出下面两个Y的坐标值
+		//模型最高顶点的Y坐标
+		_MeshVertexYMax("MeshVertexYMax",Float)=1
+		//模型最低顶点的Y坐标
+		_MeshVertexYMin("MeshVertexYMin",Float)=0
+
+		_Range("Range",Vector) = (0,0,0,0)
+
+	}
+	SubShader 
+	{
+		Pass
+		{
+		cull off
+		//Blend SrcColor OneMinusDstColor
+		//AlphaTest Less 0.5
+		CGPROGRAM
+		
+		#pragma vertex vert
+		#pragma fragment frag
+
+		#include "UnityCG.cginc"
+
+		struct m2v
+		{
+			float4 pos:POSITION;
+			float2 uv:TEXCOORD0;
+		};
+
+		struct v2f
+		{
+			float4 pos:SV_POSITION;
+			float2 uv:TEXCOORD0;
+			bool isShow:TEXCOORD1;
+		};
+
+		sampler2D _BaseTex;
+		float4 _BaseTex_ST;
+		float _Progress;
+		float _MeshVertexYMax;
+		float _MeshVertexYMin;
+		float4 _Range;
+		v2f vert(m2v v)
+		{
+			v2f f;
+			if(v.pos.y>=(_MeshVertexYMin+_Progress*(_MeshVertexYMax-_MeshVertexYMin)))
+			{
+				//消除progress=1时，最高顶点判断为false的情况
+				f.isShow=(_Progress==1);
+				//f.isShow=false;
+			}
+			else
+			{
+				f.isShow=true;
+				//f.uv = v.uv.xy * _BaseTex_ST.xy + _BaseTex_ST.zw;
+			}
+			f.uv = v.uv.xy;
+			//f.isShow = mul(unity_ObjectToWorld,v.pos).y<_Range.y;
+			f.pos=UnityObjectToClipPos(v.pos);
+			return f;
+		}
+
+		float4 frag(v2f f):SV_Target
+		{
+			float4 col= fixed4(tex2D(_BaseTex, f.uv).rgb,1);
+			if(!f.isShow){
+				discard;
+			}
+			return col;
+		}
+
+		ENDCG
+		}
+		
+	}
+	FallBack "Diffuse"
+}
