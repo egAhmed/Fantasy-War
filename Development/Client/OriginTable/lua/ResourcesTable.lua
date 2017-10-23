8d803441f00251bab0a3cdace9a35579416d0990
finalsola <122825885@qq.com>
2017-10-23 20:19:12 +0800

    AI配置表

--- Development/Client/OriginTable/lua/ResourcesTable.lua
+++ Development/Client/OriginTable/lua/ResourcesTable.lua
@@ -1,8 +1,8 @@
 module("Data")
 ResourcesTable = {
-[1001] = {id=1001,type="人族步兵",attack=10,HP=100,Range=3,atkSpeed=1,moveSpeed=5,path="Prefab/RTSCharacter/RTSHeavyInfanty/RTSHeavyInfanty"},
-[1002] = {id=1002,type="人族骑兵",attack=15,HP=75,Range=3,atkSpeed=1,moveSpeed=8,path="Prefab/RTSCharacter/RTSCavalryman/RTSCavalryman"},
-[1009] = {id=1009,type="人族农民",attack=3,HP=50,Range=3,atkSpeed=2,moveSpeed=5,path="Prefab/RTSCharacter/RTSWorker/RTSWorker"},
-[1101] = {id=1101,type="人族主城",attack=0,HP=300,Range=0,atkSpeed=0,moveSpeed=0,path="3rdPartyAssetPackage/Bitgem_RTS_Pack/Human_Buildings/Prefabs/house"},
-[1102] = {id=1102,type="人族兵营",attack=0,HP=200,Range=0,atkSpeed=0,moveSpeed=0,path="3rdPartyAssetPackage/Bitgem_RTS_Pack/Human_Buildings/Prefabs/barrack"},
+[1001] = {id=1001,type="RTSHeavyInfanty",attack=10,HP=100,Range=3,atkSpeed=1,moveSpeed=5,path="Prefab/RTSCharacter/RTSHeavyInfanty/RTSHeavyInfanty"},
+[1002] = {id=1002,type="RTSCavalryman",attack=15,HP=75,Range=3,atkSpeed=1,moveSpeed=8,path="Prefab/RTSCharacter/RTSCavalryman/RTSCavalryman"},
+[1009] = {id=1009,type="RTSWorker",attack=3,HP=50,Range=3,atkSpeed=2,moveSpeed=5,path="Prefab/RTSCharacter/RTSWorker/RTSWorker"},
+[1101] = {id=1101,type="house",attack=0,HP=300,Range=0,atkSpeed=0,moveSpeed=0,path="Prefab/RTSBuilding/Human/house"},
+[1102] = {id=1102,type="barrack",attack=0,HP=200,Range=0,atkSpeed=0,moveSpeed=0,path="Prefab/RTSBuilding/Human/barracks"},
 }
